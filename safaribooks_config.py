48ae8c1f46820d305352c771773a85da1c56ad48
TK <taepyongyang@yahoo.com>
2025-06-22 00:47:13 +0900

    Refactor configuration file for improved organization and clarity of paths and URLs

--- safaribooks_config.py
+++ safaribooks_config.py
@@ -1,18 +1,20 @@
 import os
 
-PATH = os.path.dirname(os.path.realpath(__file__))
+# Paths
+PATH         = os.path.dirname(os.path.realpath(__file__))
 COOKIES_FILE = os.path.join(PATH, "cookies.json")
 
-ORLY_BASE_HOST = "oreilly.com"  # PLEASE INSERT URL HERE
+# Base hosts
+ORLY_BASE_HOST    = "oreilly.com"  # Main O'Reilly domain
+SAFARI_BASE_HOST  = f"learning.{ORLY_BASE_HOST}"
+API_ORIGIN_HOST   = f"api.{ORLY_BASE_HOST}"
 
-SAFARI_BASE_HOST = "learning." + ORLY_BASE_HOST
-API_ORIGIN_HOST = "api." + ORLY_BASE_HOST
+# Base URLs
+ORLY_BASE_URL     = f"https://www.{ORLY_BASE_HOST}"
+SAFARI_BASE_URL   = f"https://{SAFARI_BASE_HOST}"
+API_ORIGIN_URL    = f"https://{API_ORIGIN_HOST}"
+PROFILE_URL       = f"{SAFARI_BASE_URL}/profile/"
 
-ORLY_BASE_URL = "https://www." + ORLY_BASE_HOST
-SAFARI_BASE_URL = "https://" + SAFARI_BASE_HOST
-API_ORIGIN_URL = "https://" + API_ORIGIN_HOST
-PROFILE_URL = SAFARI_BASE_URL + "/profile/"
-
-# DEBUG
+# Debug/Proxy settings
 USE_PROXY = False
-PROXIES = {"https": "https://127.0.0.1:8080"}
+PROXIES   = {"https": "https://127.0.0.1:8080"}
