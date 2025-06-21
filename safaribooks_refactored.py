10b94eb761e6e83823214fb8d96371210b387c88
TK <taepyongyang@yahoo.com>
2025-06-22 00:44:16 +0900

    Refactor argument parsing in main function for improved clarity and consistency

--- safaribooks_refactored.py
+++ safaribooks_refactored.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# coding: utf-8
 """
 SafariBooks Downloader & EPUB Generator
 
@@ -26,69 +24,61 @@ from safaribooks_process import SafariBooks
 
 # MAIN
 if __name__ == "__main__":
-    arguments = argparse.ArgumentParser(prog="safaribooks.py",
-                                        description="Download and generate an EPUB of your favorite books"
-                                                    " from Safari Books Online.",
-                                        add_help=False,
-                                        allow_abbrev=False)
 
-    login_arg_group = arguments.add_mutually_exclusive_group()
+    parser = argparse.ArgumentParser(
+        prog="safaribooks.py",
+        description="Download and generate an EPUB of your favorite books from Safari Books Online.",
+        add_help=False,
+        allow_abbrev=False
+    )
+
+    login_arg_group = parser.add_mutually_exclusive_group()
     login_arg_group.add_argument(
         "--cred", metavar="<EMAIL:PASS>", default=False,
-        help="Credentials used to perform the auth login on Safari Books Online."
-             " Es. ` --cred \"account_mail@mail.com:password01\" `."
+        help="Credentials used to perform the auth login on Safari Books Online. Es. ` --cred \"account_mail@mail.com:password01\" `."
     )
     login_arg_group.add_argument(
         "--login", action='store_true',
         help="Prompt for credentials used to perform the auth login on Safari Books Online."
     )
 
-    arguments.add_argument(
+    parser.add_argument(
         "--no-cookies", dest="no_cookies", action='store_true',
         help="Prevent your session data to be saved into `cookies.json` file."
     )
-    arguments.add_argument(
+    parser.add_argument(
         "--kindle", dest="kindle", action='store_true',
-        help="Add some CSS rules that block overflow on `table` and `pre` elements."
-             " Use this option if you're going to export the EPUB to E-Readers like Amazon Kindle."
+        help="Add some CSS rules that block overflow on `table` and `pre` elements. Use this option if you're going to export the EPUB to E-Readers like Amazon Kindle."
     )
-    arguments.add_argument(
-        "--preserve-log", dest="log", action='store_true', help="Leave the `info_XXXXXXXXXXXXX.log`"
-                                                                " file even if there isn't any error."
+    parser.add_argument(
+        "--preserve-log", dest="log", action='store_true',
+        help="Leave the `info_XXXXXXXXXXXXX.log` file even if there isn't any error."
     )
-    arguments.add_argument("--help", action="help", default=argparse.SUPPRESS, help='Show this help message.')
-    arguments.add_argument(
+    parser.add_argument("--help", action="help", default=argparse.SUPPRESS, help='Show this help message.')
+    parser.add_argument(
         "bookid", metavar='<BOOK ID>',
-        help="Book digits ID that you want to download. You can find it in the URL (X-es):"
-             " `" + SAFARI_BASE_URL + "/library/view/book-name/XXXXXXXXXXXXX/`"
+        help=f"Book digits ID that you want to download. You can find it in the URL (X-es): `{SAFARI_BASE_URL}/library/view/book-name/XXXXXXXXXXXXX/`"
     )
 
-    args_parsed = arguments.parse_args()
-    if args_parsed.cred or args_parsed.login:
-        user_email = ""
-        pre_cred = ""
-
-        if args_parsed.cred:
-            pre_cred = args_parsed.cred
-
+    args = parser.parse_args()
+    if args.cred or args.login:
+        if args.cred:
+            pre_cred = args.cred
+            user_email = ""
         else:
             user_email = input("Email: ")
             passwd = getpass.getpass("Password: ")
-            pre_cred = user_email + ":" + passwd
+            pre_cred = f"{user_email}:{passwd}"
 
         parsed_cred = SafariBooks.parse_cred(pre_cred)
-
         if not parsed_cred:
-            arguments.error("invalid credential: %s" % (
-                args_parsed.cred if args_parsed.cred else (user_email + ":*******")
-            ))
-
-        args_parsed.cred = parsed_cred
-
+            parser.error(f"invalid credential: {args.cred if args.cred else user_email + ':*******'}")
+            sys.exit(1)
+        args.cred = parsed_cred
     else:
-        if args_parsed.no_cookies:
-            arguments.error("invalid option: `--no-cookies` is valid only if you use the `--cred` option")
+        if args.no_cookies:
+            parser.error("invalid option: `--no-cookies` is valid only if you use the `--cred` option")
+            sys.exit(1)
 
-    SafariBooks(args_parsed)
-    # Hint: do you want to download more then one book once, initialized more than one instance of `SafariBooks`...
+    SafariBooks(args)
     sys.exit(0)
